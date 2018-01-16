dbc65c1699b6ca07035b60f5fb29d4b8eb8aa0ac
Nolan Hughes <nhughes987@gmail.com>
2018-01-16 00:26:13 +0000

    automatically backed up by learn

--- db/schema.rb
+++ db/schema.rb
@@ -36,10 +36,6 @@ ActiveRecord::Schema.define(version: 3) do
     t.string "location"
     t.string "theme"
     t.float "price"
-    t.boolean "family_friendly"
-    t.datetime "opening_date"
-    t.datetime "closing_date"
-    t.text "description"
   end
 
 end
