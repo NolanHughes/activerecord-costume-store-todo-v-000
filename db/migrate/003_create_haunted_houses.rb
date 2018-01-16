893d5477a62aaea76faa93e35a70e559ab484d25
Nolan Hughes <nhughes987@gmail.com>
2018-01-16 00:24:44 +0000

    automatically backed up by learn

--- db/migrate/003_create_haunted_houses.rb
+++ db/migrate/003_create_haunted_houses.rb
@@ -4,7 +4,7 @@ class CreateHauntedHouses < ActiveRecord::Migration[4.2]
     create_table :haunted_houses do |t|
       t.string :name
       t.string :location
-      # t.string :theme
+      t.string :theme
       # t.float :price
       # t.boolena :family_friendly
       # t.timestamp :opening_date
