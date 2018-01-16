6adbda6a75322ec666d6de379fdbe25a4cb1c057
Nolan Hughes <nhughes987@gmail.com>
2018-01-16 00:17:02 +0000

    automatically backed up by learn

--- db/migrate/002_create_costume_stores.rb
+++ db/migrate/002_create_costume_stores.rb
@@ -6,7 +6,7 @@ class CreateCostumeStores < ActiveRecord::Migration[4.2]
       t.string :location
       t.integer :costume_inventory
       t.integer :num_of_employees
-      still_in_business
+      t.boolean :still_in_business
       opening_time
       closing_time
     end
