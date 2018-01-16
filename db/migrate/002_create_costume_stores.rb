e61978e8b3662e1c3d019ee6aab68a7738046738
Nolan Hughes <nhughes987@gmail.com>
2018-01-16 00:15:45 +0000

    automatically backed up by learn

--- db/migrate/002_create_costume_stores.rb
+++ db/migrate/002_create_costume_stores.rb
@@ -2,7 +2,13 @@
 class CreateCostumeStores < ActiveRecord::Migration[4.2]
   def change
     create_table :costume_stores do |t|
-      
+      name
+      location
+      costume_inventory
+      number of employees
+      whether or not its still in business
+      opening_time
+      closing_time
     end
   end
 end
